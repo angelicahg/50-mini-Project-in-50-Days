8c11fecabf5625b0f4093a207abc43f371a19144
Angelica Herrera <herrerangelik@gmail.com>
2021-09-16 22:57:38 -0500

    entrar

--- script.js
+++ script.js
@@ -13,40 +13,60 @@ function scrollUp() {
   }
 }
 
+//search
+/* inputSearch = document.getElementById("inputSearch")
+
+function buscador_interno() {
+
+  filter = inputSearch.value.toUpperCase();
+  li = box_search.getElementByTagName("li");
+
+  //recorriendo elementos
+
+
+  for (i = 0; i < li.length; i++) {
+
+  }
+
+
+
+}
+ */
+/* var buscador = $("#inicio").Datatable();
+$("#input-search").keyup(function () {
+  buscador.search($(this).val()).draw();
+  if ($(".project").val() == "") {
+    $(".project").fadeOut(100);
+  } else {
+    $(".project").fadeIn(100);
+  }
+}) */
+const productos = [
+  { nombre: 'expading', valor: 1 },
+  { nombre: 'expading', valor: 3 },
+]
+
+const formulario = document.querySelector('#formulario');
+const boton = document.querySelector('#boton');
+const filtrar = () => {
+
+  const texto = formulario.value.toLowerCase();
+  for (let producto of productos) {
+    let nombre = producto.nombre.toLowerCase();
+    if (nombre.indexOf(texto) !== -1) {
+      resultado.innerHTML += `
+<li> ${producto.nombre} - valor: ${producto.nombre}</li>
+`
+    }
+
+  }
+}
+if (resultado.innerHTML === '') {
+  resultado.innerHTML += `
+  <li> no encontrado...</li>
+  `
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-$(document).ready(function () {
-
-  $('.fas').click(function () {
-    $('body, html').animate({
-      scrollTop: '0px'
-
-
-    }, 300);
-
-
-  });
-
-
-
-}); */
